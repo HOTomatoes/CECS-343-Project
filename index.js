c4edea7b79e9dcd5b160608c1cf01a4e3ebfbe92
HOTomatoes <collegewilly@gmail.com>
2024-11-04 18:03:10 -0800

    Update index.js
    updated logic for matchmaking functionality

--- index.js
+++ index.js
@@ -16,13 +16,37 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// Store connected players
+let players = [];
+const MAX_PLAYERS = 4;
+
 // Handle WebSocket connections
 io.on('connection', (socket) => {
   console.log('New client connected');
 
-  // Handle player movements, matches, etc. here
+  // Handle "Find Game" event
+  socket.on('findGame', () => {
+    if (players.length < MAX_PLAYERS) {
+      // Add player to the list
+      players.push(socket.id);
+      console.log(`Player ${socket.id} joined. Total players: ${players.length}`);
+
+      // Start the game when we have enough players
+      if (players.length === MAX_PLAYERS) {
+        console.log('Starting game...');
+        io.emit('startGame', { players }); // Notify all players to start the game
+      }
+    } else {
+      socket.emit('matchmakingFailed', { message: 'Game is full, try again later.' });
+      socket.disconnect(); // Disconnect if the game is full
+    }
+  });
+
+  // Remove player on disconnect
   socket.on('disconnect', () => {
-    console.log('Client disconnected');
+    console.log(`Player ${socket.id} disconnected`);
+    players = players.filter(player => player !== socket.id);
+    io.emit('playerDisconnected', { id: socket.id }); // Notify others
   });
 });
 
