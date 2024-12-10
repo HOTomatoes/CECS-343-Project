e4481d51aee9b4499f65db6899c92d4428ac6687
HOTomatoes <collegewilly@gmail.com>
2024-12-09 20:06:38 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,35 @@
+const express = require('express');
+const path  = require('path');
+const http = require('http');
+const { Server } = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from the public folder
+app.use(express.static('public'));
+
+// Default route to serve index.html
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Handle WebSocket connections
+io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
+
+    socket.on('message', (data) => {
+        console.log('Message received:', data);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', socket.id);
+    });
+});
+
+// Start the server
+const PORT = 3000;
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
